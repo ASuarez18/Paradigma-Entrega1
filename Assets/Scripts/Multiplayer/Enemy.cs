ed6cfff7cf5184b6ea4722dc67321b551d8e5fb8
Benjamin Cruz <A01747811@tec.mx>
2024-09-12 14:54:00 -0600

    Update Enemy.cs

--- Assets/Scripts/Multiplayer/Enemy.cs
+++ Assets/Scripts/Multiplayer/Enemy.cs
@@ -6,15 +6,19 @@ public class Enemy : MonoBehaviour
 {
     Animator animator;
     int Transicion = 0;
+    AudioSource audioSource;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         Transicion += 1;
+        audioSource.PlayOneShot(audioSource.clip);
         animator.SetInteger("Transicion", Transicion);
+
     }
 }
