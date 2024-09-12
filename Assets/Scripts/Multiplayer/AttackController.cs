c7c69f30e6e82bdde429b13c31dabdfb7bf38b9b
Benjamin Cruz <A01747811@tec.mx>
2024-09-12 14:53:43 -0600

    Update AttackController.cs

--- Assets/Scripts/Multiplayer/AttackController.cs
+++ Assets/Scripts/Multiplayer/AttackController.cs
@@ -6,11 +6,15 @@ public class AttackController : MonoBehaviour
 {
     Activador activador;
     Movema movemagia;
+    public GameObject Audio;
+
+    AudioSource audioAttack;
+
     void Start()
     {
         activador = GetComponent<Activador>();
         movemagia = GetComponent<Movema>();
-
+        audioAttack = Audio.GetComponent<AudioSource>();
     }
 
 
@@ -18,7 +22,8 @@ public class AttackController : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
+        {   
+            audioAttack.PlayOneShot(audioAttack.clip);
             activador.Actobjeto();
             movemagia.Move();
         }
